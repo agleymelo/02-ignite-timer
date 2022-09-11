2c44d10780c483a1dcba97fcd5e993ae140da88f
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-11 20:46:46 -0300

    Estrutura da aplicacao - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
-  return <div className="App"></div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div className="App"></div>
+    </ThemeProvider>
+  )
 }
