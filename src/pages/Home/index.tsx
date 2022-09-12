9b121cf00cd7ff7440af469f1726b0618c1c2123
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-12 19:22:55 -0300

    Paginas e rotas - Pagina: Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">I Will work in</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">during</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          To start
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
